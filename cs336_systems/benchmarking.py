559d7f3adb1cbd86853ff4088b27676ccd73ec3f
chatrdh <crdhuware@gmail.com>
2025-12-11 14:05:25 +0000

    dependencies bug

--- cs336_systems/benchmarking.py
+++ cs336_systems/benchmarking.py
@@ -1,1 +1,9 @@
-import cs336_basics.model
+import torch
+
+from cs336_basics.model import BasicsTransformerLM
+
+
+model = BasicsTransformerLM(vocab_size=10000,context_length=256,d_model=768,num_layers=12,num_heads=12,d_ff=3072,rope_theta=10000.0)
+
+def benchmark():
+    pass
